500833e5f40481f5402dae72f938c36b1c6ee1ed
PrathameshChaughule <pvc14102002@gmail.com>
2025-11-26 22:57:37 +0530

    feat: Home Responsive

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 let lastScrollY = window.scrollY;
 const nav = document.getElementById("nav");
 const date = document.getElementById("date");
+const item = document.getElementById("item");
+const indi = document.querySelector(".indi");
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > lastScrollY) {
     nav.classList.add("hide");
+    top2.classList.add("hide1");
+    indi.style.marginTop = "2vw";
   } else {
     nav.classList.remove("hide");
+    top2.classList.remove("hide1");
+    indi.style.marginTop = "9vw";
   }
   lastScrollY = window.scrollY;
 });
@@ -39,3 +45,27 @@ function showDate() {
 }
 
 showDate();
+
+function menu() {
+  document.querySelector(".social").classList.toggle("show");
+}
+
+let mobileMenu = document.querySelector(".mobileMenu");
+let top1 = document.querySelector(".top1");
+let top2 = document.querySelector(".top");
+
+mobileMenu.style.maxHeight = "0px";
+
+function menu() {
+  if (mobileMenu.style.maxHeight == "0px") {
+    mobileMenu.style.maxHeight = "350px";
+    top1.style.padding = "10px";
+    top2.style.gap = "1vw";
+    top2.style.animation = "bounceMenu 0.8s ease-in-out";
+  } else {
+    mobileMenu.style.maxHeight = "0px";
+    top1.style.padding = "0px";
+    top2.style.gap = "0vw";
+    top2.style.animation = "bounceMenu1 0.8s ease-in-out";
+  }
+}
